9ef7f20b8394c190c511643ce36477e39558238b
wideyu <wideyu@gmail.com>
2022-03-14 15:18:40 +0800

    Update trojan_monitor.sh

--- trojan_monitor.sh
+++ trojan_monitor.sh
@@ -1,11 +1,13 @@
 #!/bin/sh 
 
 pname=$(netstat -nlp | grep -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\:1234" | awk '{print $7}')
-[ (echo "$pname" | grep -q trojan) ] || {
+if echo "$pname" | grep -q trojan ; then
+        killall -q -9 ssr-monitor
+else
 	pid=$(echo "$pname" | sed -e "s/\/.*//g")
 	logger -t $0 "Start trojan ..."
 	kill -9 $pid
 	killall -q -9 ssr-monitor
 	export SSL_CERT_FILE=/etc/cacert.pem
 	/usr/bin/trojan -c /etc/trojan_config.json 2>&1 | logger -t "trojan" &
-}
+fi
